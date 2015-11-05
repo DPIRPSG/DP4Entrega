bf7d6ffcc8956871311fde67685b9fbeffc86b8e
migrodcab <miguel.rc95@gmail.com>
2015-11-05 17:11:02 +0100

    Query C3 y C4

--- WorkSpace/Acme-Supermarket/src/main/java/repositories/ItemRepository.java
+++ WorkSpace/Acme-Supermarket/src/main/java/repositories/ItemRepository.java
@@ -1,6 +1,9 @@
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Item;
@@ -8,4 +11,9 @@ import domain.Item;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
+	@Query("select i from Item i where i.deleted is false and i.sku IN (select oi.sku from OrderItem oi join oi.order o where o.deliveryMoment is not null group by oi.sku having max(oi.units) = (select max(oi.units) from OrderItem oi));")
+	Collection<Item> itemBestSelling();
+	
+	@Query("select i from Item i where i.deleted is false and i.sku IN (select oi.sku from OrderItem oi join oi.order o where o.deliveryMoment is not null group by oi.sku having min(oi.units) = (select min(oi.units) from OrderItem oi));")
+	Collection<Item> itemWorstSelling();
 }
