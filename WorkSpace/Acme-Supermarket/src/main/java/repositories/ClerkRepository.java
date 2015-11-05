28592e1527ce373a5d956051c35fcc94fa2aa68a
migrodcab <miguel.rc95@gmail.com>
2015-11-05 17:15:08 +0100

    Query B1 y B2

--- WorkSpace/Acme-Supermarket/src/main/java/repositories/ClerkRepository.java
+++ WorkSpace/Acme-Supermarket/src/main/java/repositories/ClerkRepository.java
@@ -1,6 +1,9 @@
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Clerk;
@@ -8,4 +11,9 @@ import domain.Clerk;
 @Repository
 public interface ClerkRepository extends JpaRepository<Clerk, Integer> {
 
+	@Query("select c1 from Order o1 join o1.clerk c1 where o1.deliveryMoment is not null group by c1 having count(o1) >= all(select count(o2) from Clerk c2 join c2.orders o2 where o2.deliveryMoment is not null group by c2);")
+	Collection<Clerk> clerkServerMoreOrders();
+	
+	@Query("select c1 from Order o1 join o1.clerk c1 where o1.deliveryMoment is not null group by c1 having count(o1) <= all(select count(o2) from Clerk c2 join c2.orders o2 where o2.deliveryMoment is not null group by c2);")
+	Collection<Clerk> clerkServerLessOrders();
 }
