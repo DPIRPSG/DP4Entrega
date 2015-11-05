5bc362c213dea55633fd67158e479ad902549802
migrodcab <miguel.rc95@gmail.com>
2015-11-05 16:49:51 +0100

    Query C1 y C2

--- WorkSpace/Acme-Supermarket/src/main/java/repositories/ConsumerRepository.java
+++ WorkSpace/Acme-Supermarket/src/main/java/repositories/ConsumerRepository.java
@@ -1,6 +1,9 @@
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Consumer;
@@ -8,4 +11,9 @@ import domain.Consumer;
 @Repository
 public interface ConsumerRepository extends JpaRepository<Consumer, Integer> {
 
-}
+	@Query("select c from Consumer c join c.orders o group by c having count(c.orders.size) = (select max(c.orders.size) from Consumer c);")
+	Collection<Consumer> findConsumerMoreOrders();
+	
+	@Query("select c from Consumer c join c.orders o where o.cancelMoment is null group by c having max(o.amount) = (select max(o.amount) from Order o);")
+	Collection<Consumer> findConsumerSpentMoreMoney();
+	}
