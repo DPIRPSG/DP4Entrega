8e66441aa437ab294fb09c15789afdad67ed05e4
migrodcab <miguel.rc95@gmail.com>
2015-11-05 16:39:27 +0100

    Todos repos 2

---
+++ WorkSpace/Acme-Supermarket/src/main/java/repositories/ContentRepository.java
@@ -0,0 +1,11 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.Content;
+
+@Repository
+public interface ContentRepository extends JpaRepository<Content, Integer> {
+
+}
