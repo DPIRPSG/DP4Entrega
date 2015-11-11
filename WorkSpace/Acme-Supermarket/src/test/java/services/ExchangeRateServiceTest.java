9cd725f26e5a847ba5e3988ce9a8fa050a740584
guillermo-ag-95 <guillermo.ag.95@gmail.com>
2015-11-11 20:25:02 +0100

    Estructura clases de prueba hechas
    Esto incluye: Creación de Source Folders, inclusión del fichero
    AbstractTest, estructura de los ficheros de test terminadas siguiendo
    los pasos del PPT.

---
+++ WorkSpace/Acme-Supermarket/src/test/java/services/ExchangeRateServiceTest.java
@@ -0,0 +1,30 @@
+package services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import utilities.AbstractTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"classpath:spring/datasource.xml",
+	"classpath:spring/config/packages.xml"})
+@Transactional
+@TransactionConfiguration(defaultRollback = false)
+public class ExchangeRateServiceTest extends AbstractTest{
+
+	// Service under test -------------------------
+	@Autowired
+	private ExchangeRateService exchangeRateService;
+	
+	// Test ---------------------------------------
+	@Test
+	public void testExchangeRate1(){
+		
+	}
+}
